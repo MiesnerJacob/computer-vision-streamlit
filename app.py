cacecf2b4e30fb7c2622fd9eb5e476d1d70088d2
miesnerjacob <miesner.jacob@gmail.com>
2022-09-01 10:21:47 -0400

    deactivated captioning, changed example file names

--- app.py
+++ app.py
@@ -42,16 +42,16 @@ def load_image_classifier():
 def load_hand_gesture_classifier():
     return HandGestureClassification()
 
-@st.cache(allow_output_mutation=True)
-def load_image_captioning():
-    return ImageCaptioning()
+# @st.cache(allow_output_mutation=True)
+# def load_image_captioning():
+#     return ImageCaptioning()
 
 
 video_object_detection = load_video_object_detection()
 image_object_detection = load_image_object_detection()
 image_classifier = load_image_classifier()
 hand_gesture_classifier = load_hand_gesture_classifier()
-image_captioning = load_image_captioning()
+# image_captioning = load_image_captioning()
 
 image_examples = {'Traffic': 'examples/Traffic.jpeg',
                   'Barbeque': 'examples/Barbeque.jpeg',
